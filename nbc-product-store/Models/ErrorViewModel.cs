0e77bbb38d9c9d4c2f06c42b8471aa31b4f2a35e
roneilc <roneilc@outlook.com>
2026-01-21 18:48:38 -0500

    first commit

---
+++ nbc-product-store/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace nbc_product_store.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
