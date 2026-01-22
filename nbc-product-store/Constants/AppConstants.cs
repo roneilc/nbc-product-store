08274aea002ee0ed8967807f6b54058114742517
roneilc <rcabalatungan@ups.com>
2026-01-22 15:40:46 -0500

    add constants file

---
+++ nbc-product-store/Constants/AppConstants.cs
@@ -0,0 +1,23 @@
+﻿namespace nbc_product_store.Constants;
+
+public static class AppConstants
+{
+    //Urls
+    public static readonly string DUMMY_JSON_PRODUCTS_URL = "https://dummyjson.com/products";
+
+    //Controller Codes
+    public static readonly string RESPONSE_STATUS_CODE_SUCCESS = "0";
+    public static readonly string RESPONSE_STATUS_DESCRIPTION_SUCCESS = "success";
+    public static readonly string RESPONSE_STATUS_CODE_FAIL = "1";
+    public static readonly string RESPONSE_STATUS_DESCRIPTION_FAIL = "fail";
+
+
+    //Backend API Generic
+    public static readonly string HTTP_ERROR = "HTTP_ERROR";
+
+    //Dummy Product API
+    public static readonly string DUMMY_JSON_FAIL = "Failed to fetch products from the Dummy Json API.";
+
+
+}
+
