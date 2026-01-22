0f6abaaec9716f44b824a7caed664ebd8ad1072c
roneilc <rcabalatungan@ups.com>
2026-01-22 15:41:11 -0500

    cart service class

---
+++ nbc-product-store/Services/CartService/CartService.cs
@@ -0,0 +1,13 @@
+using nbc_product_store.Constants;
+using nbc_product_store.Models;
+using Newtonsoft.Json;
+
+namespace nbc_product_store.Services;
+
+public class CartService : ICartService
+{
+    public void AddItemToCart()
+    {   
+    
+    }
+}
