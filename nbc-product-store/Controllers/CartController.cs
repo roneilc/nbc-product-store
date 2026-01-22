61a46e7c9cf1433804c5bc7e938fbae38010e324
roneilc <rcabalatungan@ups.com>
2026-01-22 16:49:20 -0500

    Add CartController

--- nbc-product-store/Controllers/CartController.cs
+++ nbc-product-store/Controllers/CartController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using nbc_product_store.Models;
 using nbc_product_store.Services;
 using nbc_product_store.Constants;
+using nbc_product_store.Models.Cart;
 
 namespace nbc_product_store.Controllers;
 
@@ -15,6 +16,25 @@ public class CartController : Controller
     public CartController(ICartService cartService)
     {
         _cartService = cartService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddItem([FromBody] CartRequest request)
+    {
+        if (request == null || request.ProductId <= 0 || request.Quantity <= 0)
+        {
+            return BadRequest("Invalid request");
+        }
+
+        await _cartService.AddItemToCart(request.ProductId, request.Quantity);
+        return Ok("Item added to cart");
+    }
+
+    [HttpGet]
+    public IActionResult GetCart()
+    {
+        var items = _cartService.GetCartItems();
+        return Ok(items);
     }
 
 }
