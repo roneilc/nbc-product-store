2728efe1bab5303489cef382898050ec9a0458c0
roneilc <rcabalatungan@ups.com>
2026-01-22 15:40:59 -0500

    error object model

---
+++ nbc-product-store/Models/Error/ServiceError.cs
@@ -0,0 +1,16 @@
+﻿namespace Mc4b.Models.Error
+{
+    public class ServiceError
+    {
+        public string ErrorCode;
+        public string ErrorMessage;
+
+        public ServiceError() { }
+        public ServiceError(string errorCode, string errorMessage)
+        {
+            ErrorCode = errorCode;
+            ErrorMessage = errorMessage;
+        }
+
+    }
+}
