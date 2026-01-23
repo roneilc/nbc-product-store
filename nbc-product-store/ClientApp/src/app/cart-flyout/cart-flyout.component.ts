9261cf0d7e9af6ec8c05b341eb9745f8fb0a4cfb
roneilc <rcabalatungan@ups.com>
2026-01-23 15:43:15 -0500

    implement cart flyout

---
+++ nbc-product-store/ClientApp/src/app/cart-flyout/cart-flyout.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, Output, EventEmitter, signal } from '@angular/core';
+import { CartItem } from '../models/cart.model';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cart-flyout',
+  templateUrl: './cart-flyout.component.html',
+  styleUrls: ['./cart-flyout.component.css'],
+  imports: [CommonModule],
+})
+export class CartFlyoutComponent {
+  @Input() showCart: boolean = false;
+  @Input() cartItems: CartItem[] = [];
+  @Input() cartCount: number = 0;
+  @Input() cartTotal: number = 0;
+  @Output() close = new EventEmitter<void>();
+
+  onClose() {
+    this.close.emit();
+  }
+}
