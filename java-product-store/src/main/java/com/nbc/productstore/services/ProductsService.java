2894d3dfe7e1c51619d77e778d4064adfdf90de5
roneilc <rcabalatungan@ups.com>
2026-01-23 16:02:14 -0500

    java products service implementation

---
+++ java-product-store/src/main/java/com/nbc/productstore/services/ProductsService.java
@@ -0,0 +1,9 @@
+package com.nbc.productstore.services;
+
+import com.nbc.productstore.models.Product;
+
+import java.util.List;
+
+public interface ProductsService {
+    List<Product> getProducts();
+}
