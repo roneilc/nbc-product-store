2d2bdc03306191739edf9b433e248cfa459be138
roneilc <rcabalatungan@ups.com>
2026-01-23 09:06:11 -0500

    add java app

---
+++ java-product-store/src/main/java/com/example/productstore/ProductStoreApplication.java
@@ -0,0 +1,11 @@
+package com.example.productstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductStoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductStoreApplication.class, args);
+    }
+}
